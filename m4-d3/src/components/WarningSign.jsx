3e47f80475547864ecc4a0ef9c19900589f8f817
CianMW <markwick.cian@gmail.com>
2021-09-29 13:43:17 +0100

    Setup-Repo

---
+++ m4-d3/src/components/WarningSign.jsx
@@ -0,0 +1,22 @@
+import Alert from 'react-bootstrap/Alert'
+
+const WarningAlert = () = {
+
+        return (
+        [
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark',
+        ].map((variant, idx) => (
+            <Alert key={idx} variant={variant}>
+            This is a {variant} alert—check it out!
+            </Alert>
+        ));
+        )
+
+}
